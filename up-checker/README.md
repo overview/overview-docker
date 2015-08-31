a9f0385c42a26cf83f32cf8c02a987d8356021c2
Jonas Karlsson <thejonas@gmail.com>
2015-08-31 17:58:58 -0400

    Also check for database

--- up-checker/README.md
+++ up-checker/README.md
@@ -1,5 +1,10 @@
 Up Checker
 ==========
 
-Utility to check if the web frontend is running.
+Utility to check if a service is running. Currently two options are available:
+
+  - `frontend` Checks if the Overview server is running at the IP address in `DOCKER_HOST`, or `localhost`, if not set.
+  - `database` Checks if the Overview database is available at `overview-database`. Link the database containeer appropriately when running
+
+
 
--- up-checker/upcheck.sh
+++ up-checker/upcheck.sh
@@ -5,23 +5,60 @@
 ## localhost, if not set.
 ## 
 
-service_is_down=true
 
-PLUGIN_HOST=$(echo ${DOCKER_HOST:-localhost} | sed 's/[a-zA-Z]*:\/\/\(.*\):.*/\1/')
 
-url=http://${PLUGIN_HOST}:9000
+wait_for_frontend() {
 
-while [ ${service_is_down} == true ]; do
-  echo Waiting for server at $url
-  sleep 1
+  service_is_down=true
 
-  wget -q -O /dev/null $url
+  PLUGIN_HOST=$(echo ${DOCKER_HOST:-localhost} | sed 's/[a-zA-Z]*:\/\/\(.*\):.*/\1/')
 
-  if [ $? == 0 ]; then
-    service_is_down=false
-  fi
-done
+  url=http://${PLUGIN_HOST}:9000
 
-echo The server is available at $url
+  while [ ${service_is_down} == true ]; do
+    echo Waiting for server at $url
+    sleep 1
+
+    wget -q -O /dev/null $url
+
+    if [ $? == 0 ]; then
+      service_is_down=false
+    fi
+  done
+
+  echo The server is available at $url
+}
+
+wait_for_database() {
+  export PGUSER=overview
+  export PGPASSWORD=overview
+  
+  service_is_down=true
+
+  while [ ${service_is_down} == true ]; do 
+    echo Waiting for overview-database
+    sleep 1
+
+    psql -h overview-database -p 5432 -l &> /dev/null
+
+    if [ $? == 0 ]; then
+      service_is_down=false
+    fi
+  done
+
+  echo overview-database is available
+}
+
+
+if [ "$1" == "frontend" ]; then
+  wait_for_frontend
+elif [ "$1" == "database" ]; then
+  wait_for_database
+else 
+  echo "Usage: upcheck [frontend|database]"
+fi
+
+
+
 
 
