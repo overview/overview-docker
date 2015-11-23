952fbb61e50d2c71f399bd39ffbcf81b848f6c11
Adam Hooper <adam@adamhooper.com>
2015-11-23 09:47:06 -0500

    Nix all environment variables

--- docker-build.sh
+++ docker-build.sh
@@ -3,13 +3,13 @@
 cd "$(dirname "$0")"
 
 # Support services
-docker build --rm -t overview/database database
-docker build --rm -t overview/searchindex searchindex
+docker build --no-cache --rm -t overview/database database
+docker build --no-cache --rm -t overview/searchindex searchindex
 
 # Create a base with everything built and staged
-docker build --rm -t overview/overview-base overview-base
+docker build --no-cache --rm -t overview/overview-base overview-base
 
-docker build --rm -t overview/db-evolution-applier db-evolution-applier
-docker build --rm -t overview/documentset-worker documentset-worker
-docker build --rm -t overview/worker worker
-docker build --rm -t overview/web web
+docker build --no-cache --rm -t overview/db-evolution-applier db-evolution-applier
+docker build --no-cache --rm -t overview/documentset-worker documentset-worker
+docker build --no-cache --rm -t overview/worker worker
+docker build --no-cache --rm -t overview/web web
