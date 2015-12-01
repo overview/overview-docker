a0c1dcaafde0523ccac2a97c8d8b31dcaba18156
Adam Hooper <adam@adamhooper.com>
2015-12-01 12:13:47 -0500

    Set up dev/test environments in Postgres

--- database/overview-setup.sql
+++ database/overview-setup.sql
@@ -1,4 +1,4 @@
-
-
-CREATE ROLE overview LOGIN PASSWORD 'overview';
+CREATE ROLE overview LOGIN;
 CREATE DATABASE overview OWNER overview;
+CREATE DATABASE overview-dev OWNER overview;
+CREATE DATABASE overview-test OWNER overview;
