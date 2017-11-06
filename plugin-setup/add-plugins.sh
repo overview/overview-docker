1a4db4fa75f8aa7ea16ab38b1b88c6560f912e7d
Adam Hooper <adam@adamhooper.com>
2017-11-06 11:32:12 -0500

    Make plugin-setup use OV_DOMAIN_NAME

--- plugin-setup/add-plugins.sh
+++ plugin-setup/add-plugins.sh
@@ -1,13 +1,39 @@
-#!/bin/bash
+#!/bin/sh
 
-export PGUSER=overview
-export PGPASSWORD=overview
+# Find URLs of plugins, to set as iframes. The plugins will all have the
+# Overview server set at 'http://overview-web:9000' (an internal address).
+if [ -n "$OV_DOMAIN_NAME" ]; then
+  # Best: `OV_DOMAIN_NAME` is set. We will auto-generate URLs based on that.
 
-# Extract the IP address from $DOCKER_HOST
-# If $DOCKER_HOST is not set, use localhost
-PLUGIN_HOST=${DOCKER_HOST:-localhost}
+  SUBDOMAIN_ENDINGS="plugin-entity-filter plugin-file-browser plugin-multi-search plugin-word-cloud"
+  dns_name="$(echo "$OV_DOMAIN_NAME" | cut -d. -f1)"
+  dns_domain="$(echo "$OV_DOMAIN_NAME" | cut -d. -f2-)"
 
-psql -h overview-database -p 5432 <<EOF
+  ENTITY_FILTER_URL="https://$dns_name-plugin-entity-filter.$dns_domain"
+  MULTI_SEARCH_URL="https://$dns_name-plugin-multi-search.$dns_domain"
+  FILE_BROWSER_URL="https://$dns_name-plugin-file-browser.$dns_domain"
+  WORD_CLOUD_URL="https://$dns_name-plugin-word-cloud.$dns_domain"
+elif [ -n "$DOCKER_HOST" ]; then
+  echo 'DEPRECATED: do not set $DOCKER_HOST any more. Use $OV_DOMAIN_NAME instead.' 1>&2
+  ENTITY_FILTER_URL="http://$DOCKER_HOST:3001"
+  MULTI_SEARCH_URL="http://$DOCKER_HOST:3004"
+  FILE_BROWSER_URL="http://$DOCKER_HOST:3003"
+  WORD_CLOUD_URL="http://$DOCKER_HOST:3000"
+else
+  ENTITY_FILTER_URL="http://localhost:3001"
+  MULTI_SEARCH_URL="http://localhost:3004"
+  FILE_BROWSER_URL="http://localhost:3003"
+  WORD_CLOUD_URL="http://localhost:3000"
+fi
+
+echo "Setting up plugin URLs."
+echo "Word Cloud: $WORD_CLOUD_URL"
+echo "Entity Filter: $ENTITY_FILTER_URL"
+echo "File Browser: $FILE_BROWSER_URL"
+echo "Multi Search: $MULTI_SEARCH_URL"
+echo "These are internal URLs: they will give errors if you browse to them. They are printed here to help with debugging."
+
+PGUSER=overview PGPASSWORD=overview psql -h overview-database -p 5432 <<EOF
 
 DELETE FROM plugin WHERE id IN (
   '90115025-9C9E-4BA7-A9B7-6C9BB69A27BF',
