20d5e46d19fe197c086f12ed0bf078e7b04f74e2
Adam Hooper <adam@adamhooper.com>
2017-11-10 13:47:45 -0500

    Revamp Docker images
    This should save 50% of space or more.
    
    Unfortunately, libreoffice conversion from text or ODT does not work
    yet. Debug it!

--- test/docker-build.sh
+++ test/docker-build.sh
@@ -5,11 +5,11 @@ set -e
 cd "$(dirname "$0")"
 
 # Support services
-docker build --no-cache --rm -t overview/database ../database
+docker build --rm -t overview/database ../database
 
 # Create a base with everything built and staged
-docker build --no-cache --rm -t overview/overview-base ../overview-base
+docker build --rm -t overview/overview-base ../overview-base
 
-docker build --no-cache --rm -t overview/db-evolution-applier ../db-evolution-applier
-docker build --no-cache --rm -t overview/worker ../worker
-docker build --no-cache --rm -t overview/web ../web
+docker build --rm -t overview/db-evolution-applier ../db-evolution-applier
+docker build --rm -t overview/worker ../worker
+docker build --rm -t overview/web ../web
